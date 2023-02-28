cfdfb2be756bf3b62b4b7dc0226a090050917788
PeaceDeathDullBoyJob <101503928+PeaceDeathDullBoyJob@users.noreply.github.com>
2023-02-28 05:47:16 +0300

    poop

---
+++ Hospital Management System/Hospital Management System/Models/Appointment.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+namespace Hospital_Management_System.Models
+{
+    public class Appointment
+    {
+        public int Id { get; set; }
+
+        public Patient Patient { get; set; }
+        [Display(Name = "Пациент")]
+        public int PatientId { get; set; }
+
+        public Doctor Doctor { get; set; }
+        [Display(Name = "Врач")]
+        public int DoctorId { get; set; }
+
+        [Display(Name = "Время приёма")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime? AppointmentDate { get; set; }
+
+        public string Problem { get; set; }
+      
+
+
+        public bool Status { get; set; }
+           
+
+    }
+}
